57f5359a8758c126bbea34c848360531a2d4b399
mohamed606 <mohamed606tanna@gmail.com>
2020-03-05 15:27:28 +0200

    adding createIncidenceMatrix

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,46 +1,53 @@
 package com.company;
 
+import javax.crypto.AEADBadTagException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Enter number of Vertices");
-        Scanner input= new Scanner(System.in);
-        int numberOfVetices= input.nextInt();
+        Scanner input = new Scanner(System.in);
+        int numberOfVetices = input.nextInt();
 
         String startVertex;
         String terminalVertex;
-        ArrayList<Vertex>graphVertices=new ArrayList<>();
-        for(int i=0;i<numberOfVetices;i++)
-        {
+        ArrayList<Vertex> graphVertices = new ArrayList<>();
+        ArrayList<Edge> graphEdges = new ArrayList<>();
+        for (int i = 0; i < numberOfVetices; i++) {
             graphVertices.add(new Vertex(input.next()));
         }
-    Graph graph=new Graph(graphVertices);
+
+
+        Graph graph = new Graph(graphVertices, graphEdges);
 
         int termination;
+        String edgeName;
 
-        while(true)
-        {
+
+        while (true) {
             System.out.println("Enter -1 to Exit");
-            termination=input.nextInt();
-            if(termination==-1)
-            {
+            termination = input.nextInt();
+            if (termination == -1) {
                 break;
             }
-            startVertex=input.next();
-            terminalVertex=input.next();
-            Vertex start=graph.findVertex( startVertex);
-            Vertex terminal =graph.findVertex(terminalVertex);
-            if(!start.equals(terminal)){
+            System.out.println("Enter Pair of vertices");
+            startVertex = input.next();
+            terminalVertex = input.next();
+            System.out.println("Enter the name of the edge");
+            edgeName = input.next();
+            Vertex start = graph.findVertex(startVertex);
+            Vertex terminal = graph.findVertex(terminalVertex);
+            graphEdges.add(new Edge(edgeName, start, terminal));
+            if (!start.equals(terminal)) {
                 start.addVertex(terminal);
                 terminal.addVertex(start);
-            }else {
+            } else {
                 start.addVertex(terminal);
             }
-        }
-//      ArrayList<Vertex>adjacencyList= graph.createAdjacencyList();
+            //  ArrayList<Vertex>adjacencyList= graph.createAdjacencyList();
 //        for(int i=0;i<adjacencyList.size();i++)
 //        {
 //            Vertex vertex = adjacencyList.get(i);
@@ -53,11 +60,13 @@ public class Main {
 //            }
 //            System.out.println();
 //        }
-        printTwoDemnsionalArray(graph.createRepresentationMatrix(),graphVertices);
+            //printTwoDemnsionalArray(graph.createRepresentationMatrix(),graphVertices);
 
+
+        }
+        printIncidenceMatrix(graph.createIncidenceMatrix(),graphVertices,graphEdges);
     }
     static void printTwoDemnsionalArray(int [][] array , ArrayList<Vertex>vertices){
-        System.out.print("  ");
         for(int i=0 ; i<vertices.size();i++){
             System.out.print(vertices.get(i).getVertexName()+" ");
         }
@@ -66,6 +75,25 @@ public class Main {
             System.out.print(vertices.get(i).getVertexName()+" ");
             for(int j=0;j<array[0].length;j++){
                 System.out.print(array[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+    static void printIncidenceMatrix(int [][] incidenceMatrix,ArrayList<Vertex>graphVertices,ArrayList<Edge>graphEdges)
+    {
+        System.out.print("  ");
+
+        for(int i=0;i<graphEdges.size();i++)
+        {
+            System.out.print(graphEdges.get(i).getEdgeName() + "  ");
+        }
+        System.out.println();
+        for(int i=0;i<graphVertices.size();i++)
+        {
+            System.out.print(graphVertices.get(i).getVertexName() +"  ");
+            for(int j=0;j<incidenceMatrix[0].length;j++)
+            {
+                System.out.print(incidenceMatrix[i][j] + " ");
             }
             System.out.println();
         }
